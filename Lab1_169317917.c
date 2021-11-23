4b2c21398b29aa863a4884f64ee962a1aab4f759
Juan Miranda Cárdenas <jmirandav2@gmail.com>
2021-11-23 01:14:35 -0300

    todas las funcionalidades complejas, listas

--- Lab1_169317917.c
+++ Lab1_169317917.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 typedef struct nodo
 {
     int posicionFila; 
@@ -11,16 +10,38 @@ typedef struct nodo
     struct nodo *siguiente;
 }Nodo;
 
+Nodo* agregar(Nodo* lst, int posicionFila, char tipo, int duracion);
+void imprimirLista(Nodo* lst);
+Nodo* filtrar(Nodo* lst_org, Nodo* lst_dst, char criterio);
+Nodo* cargarContenidoEnLista(Nodo* lst, char* nombreArchivo);
+Nodo* filtroTiempo(Nodo* lst);
+int largoLista(Nodo* lst);
+Nodo* diferencia(Nodo* lst_org, Nodo* lst_dst);
 
+int* obtenerFila(Nodo* lst);
+void escribirResultados(char* nombreArchivo, Nodo* lst_org, Nodo* lst_dst);
 
-Nodo* crearNodo(int posicionFila, char tipo, int duracion){
-    
-    Nodo* nuevo = (Nodo*) malloc(sizeof(Nodo));
-    nuevo->posicionFila = posicionFila;
-    nuevo->tipo = tipo;
-    nuevo->duracion = duracion;
-    nuevo->siguiente = NULL;
-    return nuevo;
+int main(int argc, char const *argv[])
+{
+    Nodo* listaEntrada = (Nodo*) malloc(sizeof(Nodo));
+    Nodo* listaResultado = (Nodo*) malloc(sizeof(Nodo));
+    listaEntrada = NULL;
+    listaResultado = NULL;
+
+    listaEntrada = cargarContenidoEnLista(listaEntrada, "entrada.in");
+
+    listaResultado = filtrar(listaEntrada, listaResultado, 'a');
+    listaResultado = filtrar(listaEntrada, listaResultado, 'v');
+    listaResultado = filtrar(listaEntrada, listaResultado, 'n');
+    listaResultado = filtroTiempo(listaResultado);
+    imprimirLista(listaResultado);
+
+    escribirResultados("salida.out", listaEntrada, listaResultado);
+
+    //imprimirLista(listaEntrada);
+    //printf("Largo lista: %d\n", largo(listaEntrada));
+
+    return 0;
 }
 
 Nodo* agregar(Nodo* lst, int posicionFila, char tipo, int duracion){
@@ -60,7 +81,15 @@ void imprimirLista(Nodo* lst){
         auxiliar = auxiliar->siguiente;
     }
 }
-
+/**
+ * @brief Se entrega la lista que debe ser filtrada, bajo un criterio (a, v, n).
+ * Si algun nodo coincide con el criterio, el contenido de ese nodo es copiado en una nueva lista.
+ * 
+ * @param lst_org lista original 
+ * @param lst_dst lista destino o lista nueva
+ * @param criterio critero para aplicar filtro
+ * @return Nodo* nueva lista creada a partir del criterio de selección
+ */
 Nodo* filtrar(Nodo* lst_org, Nodo* lst_dst, char criterio){
     Nodo* auxiliar;
     auxiliar = lst_org;
@@ -96,7 +125,13 @@ Nodo* filtrar(Nodo* lst_org, Nodo* lst_dst, char criterio){
     }
     return lst_dst;
 }
-
+/**
+ * @brief Carga el contenido del archivo de entrada en una lista.
+ * 
+ * @param lst 
+ * @param nombreArchivo 
+ * @return Nodo* 
+ */
 Nodo* cargarContenidoEnLista(Nodo* lst, char* nombreArchivo){
 
     FILE* archivo = fopen(nombreArchivo, "r");
@@ -130,10 +165,18 @@ Nodo* cargarContenidoEnLista(Nodo* lst, char* nombreArchivo){
         lst = agregar(lst, n_PosicionFila, tipoPublico[0], duracion);
         posicionLista++; 
     }
+
+    fclose(archivo);
     return lst;
 }
-
-Nodo* calculoTiempo(Nodo* lst){
+/**
+ * @brief Dada un lista, selecciona todos los turnos que pueden ser atendidos en la ventana de tiempo
+ * entre las 9:00 y las 14:00 o sea 300 minutos. 
+ * 
+ * @param lst 
+ * @return Nodo* 
+ */
+Nodo* filtroTiempo(Nodo* lst){
     Nodo* auxiliar;
     auxiliar = lst;
     int tiempoTotal = 0;
@@ -146,23 +189,104 @@ Nodo* calculoTiempo(Nodo* lst){
     }
     return lst;
 }
+int largoLista(Nodo* lst){
+   int i = 0;
+    Nodo* indice = lst;
+    
+    while(indice != NULL){
+        indice = indice -> siguiente;
+        i++;
+    }
 
-int main(int argc, char const *argv[])
+    return i; 
+}
+int* obtenerFila(Nodo* lst){
+    int largo = largoLista(lst), indice = 0;
+    int* fila = (int*) malloc (largo * sizeof(int));
+    Nodo* aux = lst;
+
+    while (aux != aux)
+    {
+        fila[indice] = aux->posicionFila;
+        aux = aux->siguiente;
+        indice++;
+    }
+
+    return fila;
+}
+
+Nodo* diferencia(Nodo* lst_org, Nodo* lst_dst)
 {
-    Nodo* listaEntrada = (Nodo*) malloc(sizeof(Nodo));
-    Nodo* listaResultado = (Nodo*) malloc(sizeof(Nodo));
-    listaEntrada = NULL;
-    listaResultado = NULL;
+    Nodo* aux_org = lst_org;
+    Nodo* aux_dst = lst_dst;
+    Nodo* lst_dif = (Nodo*) malloc(sizeof(Nodo));
+    lst_dif = NULL;
+    int encontrado = 0;
 
-    listaEntrada = cargarContenidoEnLista(listaEntrada, "entrada.in");
-    listaResultado = filtrar(listaEntrada, listaResultado, 'a');
-    listaResultado = filtrar(listaEntrada, listaResultado, 'v');
-    listaResultado = filtrar(listaEntrada, listaResultado, 'n');
-    listaResultado = calculoTiempo(listaResultado);
-    imprimirLista(listaResultado);
+    while (aux_org != NULL)
+    {
+        while (aux_dst != NULL)
+        {
+            if(aux_org->posicionFila == aux_dst->posicionFila)
+                encontrado++;
 
-    //imprimirLista(listaEntrada);
-    //printf("Largo lista: %d\n", largo(listaEntrada));
+            aux_dst = aux_dst->siguiente;
+        }
+   
+        if(encontrado == 0)
+            lst_dif = agregar(lst_dif, aux_org->posicionFila, aux_org->tipo, aux_org->duracion);
+        
+        aux_dst = lst_dst;
+        aux_org = aux_org->siguiente;
+        encontrado = 0;
+    }
+    
+    return lst_dif;
+}
 
-    return 0;
+void escribirResultados(char* nombreArchivo, Nodo* lst_org, Nodo* lst_dst)
+{
+    FILE* archivo = fopen(nombreArchivo, "a");
+    int indice;
+    Nodo* auxiliar = lst_org;
+    Nodo* lst_dif = (Nodo*) malloc(sizeof(Nodo));
+   
+    char c_numero[3];
+    
+    fputs("Fila: \n", archivo);
+    while (auxiliar != NULL)
+    {
+        sprintf(c_numero, "%d\n", auxiliar->posicionFila);
+        fputs(c_numero, archivo);
+
+        auxiliar = auxiliar->siguiente;
+    }
+
+    fputs("\nAtendidos: \n", archivo);
+    
+    auxiliar = lst_dst;
+
+    while (auxiliar != NULL)
+    {
+        sprintf(c_numero, "%d\n", auxiliar->posicionFila);
+        fputs(c_numero, archivo);
+
+        auxiliar = auxiliar->siguiente;
+    }
+    
+    fputs("\nNo atendidos: \n", archivo);
+    lst_dif = diferencia(lst_org, lst_dst);
+    
+    auxiliar = lst_dif;
+    
+    while (auxiliar != NULL)
+    {
+        sprintf(c_numero, "%d\n", auxiliar->posicionFila);
+        fputs(c_numero, archivo);
+
+        auxiliar = auxiliar->siguiente;
+    }
+    
+    fclose(archivo);
+
 }
